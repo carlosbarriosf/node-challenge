3085fb5596f5408f669fa4aabdd95a9824d35bf1
Carlos <carlosbarriosf6@gmail.com>
2025-11-14 09:29:28 +0100

    added changes to db connection function

--- src/Database/database.js
+++ src/Database/database.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
   try {
-    const mongoDB = await mongoose.connect(
-      "mongodb+srv://carlosbarriosf:Degradito15@cluster0.xseba8s.mongodb.net/",
-      {
-        dbName: process.env.DB_NAME,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const mongoDB = await mongoose.connect(process.env.DB_URL_CONNECTION, {
+      dbName: process.env.DB_NAME,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB connection successful");
   } catch (error) {
     console.log(error);
